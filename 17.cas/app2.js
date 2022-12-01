5b46a94a17b9b65798126fe4ec5b2a2f20b41f40
Amer <amer2014np@gmail.com>
2022-12-01 20:19:25 +0100

    homework

---
+++ 17.cas/app2.js
@@ -0,0 +1,17 @@
+//let a = 10;
+
+                        // HOISTING //
+// hoisiting predstavlja  deklarisanje promenljivih pre same dodele vrednosti  (u pozadini).
+
+
+//prilikom citanja kod da se "izvlaci" deklaracija na vrhu.
+
+
+// var prom1; da mi ne vidimo;
+prom1=23; 
+console.log(prom1);
+var prom1; // ako ne uradim ovo nije strasno
+// js to radi u pozadini prilikom citanja koda
+
+
+// povlacenje deklaracije // definisanje promenljive na vrhu
