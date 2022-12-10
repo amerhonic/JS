98d80765883541d5e5db7edba7d1d8cf887e00cd
Amer <amer2014np@gmail.com>
2022-12-10 18:56:48 +0100

    zaboravio se

---
+++ 21.cas/dm.js
@@ -0,0 +1,10 @@
+// function nesto (rec) {
+//     let nova="";
+//     let i;
+//     for (i=rec.lenght;i>=0;i--) {
+//       nova+=rec[i];  
+//     }
+//     if (nova===rec) return "jes";
+//     else return "nije";       
+// }
+
