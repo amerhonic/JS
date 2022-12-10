4f7ae0181273fff1cab037bd9b96c8f144b281c5
Amer <amer2014np@gmail.com>
2022-12-10 17:32:29 +0100

    20

--- 20.cas/app.js
+++ 20.cas/app.js
@@ -1,11 +1,52 @@
-function malih(r) {
-    let i;
-    let brojac=0;
-    for (i=0;i<r.length;i++) {
-        if (r[i]===r[i].toLowerCase() && r[i]!==" ") {
-            brojac++;
-        }
+// function malih(r) {
+//     let i;
+//     let brojac=0;
+//     for (i=0;i<r.length;i++) {
+//         if (r[i]===r[i].toLowerCase() && r[i]!==" ") {
+//             brojac++;
+//         }
+//     }
+//     return brojac;
+// } 
+// console.log(malih("vidinesto JS"));
+
+// ;function razlika(r) {
+//     let i;
+//     let brojac1=0;
+//     let brojac2=0;
+//     for (i=0;i<r.length;i++)  {
+//         if (r[i]=== r[i].toLowerCase() && r[i]!==" ") {
+//             brojac1++;
+//         }
+//         else if (r[i] === r[i].toUpperCase() && r[i] !==" " ) {
+//             brojac2++;
+//         }
+//     }
+//     if (brojac1>brojac2) {
+//         return "ima vise malih"
+//     } else return " ima vise velikih";
+// }
+//  console.log(razlika("ne zna se recenica JOkdJIFA KLj")) 
+
+// function pretvaranje (r) {
+//     let i;
+//     let r1 = "";
+//     for (i=0;i<r.length;i++) {
+//         r1+=r[i].toLowerCase()
+//     }
+//   return r1;  
+// }
+
+
+
+function last (r,k) {
+
+    for (i=0;i<=r;i++) {
+        return k[i];
     }
-    return brojac;
-} 
-console.log(malih("vidinesto JS"));
+    
+    }
+
+
+
+    
