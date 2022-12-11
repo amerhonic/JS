a0433b8f0876b2001c5480d7c924a75f8e738dee
Amer <amer2014np@gmail.com>
2022-12-11 18:36:39 +0100

    aha

---
+++ 21.cas/domaci.js
@@ -0,0 +1,45 @@
+function nesto(r,n) {
+    let x="";
+ for (i=r.length-n;i<r.length;i++) {
+    x+=r[i];
+ } return x;
+}
+ console.log(nesto("neka recenica ide" , 3));
+
+ // preko slice metode.
+ // const task = (recenica,n) {
+    //const poslednjih = recenica.slic(-n);
+   // return poslednjih;
+ //}
+
+
+//  function ima(nikad) {
+//     let nova="";
+// for (let i=0;i<nikad.length;i++) {
+//     if (nikad.length%2!==0 &&  i===nikad.length-1) {
+//         nova+=nikad[i];
+//     }
+//     if (i%2===0) {
+//         nova+=nikad[i+1]
+//     } else (nova+=nikad[i-1])
+// } return nova;
+//  }
+//   console.log (ima ("ovaj string ima nanu za odbranu"))
+
+
+// 3. zadatak 
+function user(protect)   {
+    
+    let y;
+    y=protect.indexOf("_")
+    let x;
+    z=""
+    x=protect.indexOf("@")
+for (let i = 0;i<protect.length;i++) {
+    if (email[i]==="_") {
+        r
+    }
+}
+ return protect 
+}
+console.log(user("amer_jfhdakjfa@gmail.com"))
