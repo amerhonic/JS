27cd22f155a1d91d42d02f23f5b68d336cb5a732
Amer <amer2014np@gmail.com>
2022-12-24 17:07:47 +0100

    r

---
+++ 24.cas/domaci.js
@@ -0,0 +1,16 @@
+function radijani(y) {
+    return (par * 180) / Math.PI;
+  }
+  console.log(radijani(Math.PI / 6));
+  
+  function obim(par) {
+    return 2 * r * Math.PI;
+  }
+  console.log(radijani(2));
+  
+  function povrsina(x) {
+    r = r / 2;
+    return r ** 2 * Math.PI;
+  }
+  console.log(radijani(4));
+
