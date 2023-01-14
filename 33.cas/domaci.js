dfe8f56a0b3a96d102c69104917674fd57070b02
Amer <amer2014np@gmail.com>
2023-01-14 17:27:32 +0100

    s

---
+++ 33.cas/domaci.js
@@ -0,0 +1,13 @@
+const brojevi = [-5,4,3,10,14,-5]
+const brojevi1 = brojevi.map((nekako) => nekako*(-1));
+console.log(brojevi1)
+
+const brojevi2= brojevi.map((brighton) => brighton*brighton);
+console.log(brojevi2);
+
+const brojevi3=brojevi.map((manchester) => Math.sqrt(manchester))
+console.log(brojevi3);
+
+const stringovi = ["bakir" , "manchester osvaja" ,"brighton dobija" , "marchu spomenik da se digne"];
+const string2=stringovi.map((kako) => kako.toUpperCase())
+console.log(string2);
