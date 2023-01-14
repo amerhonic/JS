b31604b2e9c34b36fffb3085992aa31e2b7dad85
Amer <amer2014np@gmail.com>
2023-01-14 19:05:37 +0100

    ja bi da platim

--- 34.acs/app.js
+++ 34.acs/app.js
@@ -18,3 +18,25 @@ const second = (arr) => {
 
 }
 console.log(second([5,2,3,1,4,5]))
+brojevi = [5,32,1,24,12,3]
+
+const brojevi1= brojevi.map((element,index) => {
+    if (index===0 || index===3 || index === 5) {
+        return element**2;
+    } else return element;
+})
+console.log(brojevi1);
+
+
+const numbers = [2,5,10,3,4,8] 
+
+const numbers2=  numbers.map((element,index,arr)=> {
+if (element%2===0) {
+    return element **2;
+} else {
+    return+((arr[index-1] +element+arr[index+1])/3).toFixed(2);
+}
+})
+console.log(numbers2);
+
+//pomocu for petlje napraviti novi niz koji ce sadrzati samo godoine vece od 17.
