c2e08317e9a02a90fb99bd063d2313861eb7cbea
Amer <amer2014np@gmail.com>
2023-01-15 18:09:18 +0100

    prvi

--- 34.acs/domaci.js
+++ 34.acs/domaci.js
@@ -1,4 +1,4 @@
-
+ 
 
 // 1.pomocu for petlje napraviti novi niz koji ce sadrzati samo godoine vece od  17.
 
@@ -8,3 +8,13 @@
 
 
 
+const godine = [2,6,22,44,16,15,12,49,24,22];
+const godinicie = (arr) => {
+    const puno = [];
+    for (let i=0;i<godine.length;i++) {
+        if (godine[i]>17) {
+            puno.push(godine[i]);
+        }
+    }return puno;
+}
+console.log(godinicie(godine))
