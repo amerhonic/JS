8299f4949c37577b45bffab48cea06609138df7d
Amer <amer2014np@gmail.com>
2023-01-15 19:01:05 +0100

    drugi jos malo

--- 34.acs/domaci.js
+++ 34.acs/domaci.js
@@ -4,7 +4,7 @@
 
 
 
-// 2. kod dzena.uzima dva argumenta salju se dva niza
+
 
 
 
@@ -18,3 +18,40 @@ const godinicie = (arr) => {
     }return puno;
 }
 console.log(godinicie(godine))
+
+
+// 2.
+// Prvi deo. Napraviti pomocni niz koji ce sadrzati elemente od oba niza.
+// Drugi deo. Napraviti jos jedan pomocni niz koji ce iz prvog dodatnog niza uzeti parne brojeve.
+// Treci deo. U drugom pomocnom nizu umetnuti na prva dva mesta 10 i 20.
+// Funkcija na kraju treba da vrati dva napravljena pomocna niza.
+
+const DATA1 = [2, 26, 38, 75, 11, 29];
+const DATA2 = ["a", "b", "c", "d", "e"];
+
+const pomoc = (a,a1) => {
+    let pomocni = []
+    const parni = []
+    let n;
+    let k=0;
+    n=DATA1.length+DATA2.length;
+
+   for (let i=0;i<DATA1.length;i++) {
+        pomocni.push(DATA1[i]);
+        
+        }
+        for (i=0;i<DATA2.length;i++) {
+            pomocni.push(DATA2[i]);
+        }
+
+        for (i=0;i<n;i++) {
+            if (pomocni[i]%2===0) {
+                parni.push(pomocni[i]);
+            }
+        }
+        return parni;
+        
+
+}
+
+console.log(pomoc(DATA1,DATA2))
