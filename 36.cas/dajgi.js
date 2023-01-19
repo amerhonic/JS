85228c987e6c62f0d3a19c97282375445af72886
Amer <amer2014np@gmail.com>
2023-01-19 21:01:19 +0100

    s

---
+++ 36.cas/dajgi.js
@@ -0,0 +1,24 @@
+function pali(palindrom) {
+    let br=palindrom;
+    let broj=palindrom;
+    
+    let cifra=0;
+   while(broj>0) {
+    broj=br%10;
+    cifra+=broj*10;
+    
+
+   }
+   if (cifra===palindrom) return "jes-"
+ 
+}
+
+
+console.log(pali(9009))
+
+
+for (let i=100;i<999;i++) {
+    for (let j=100;j<999;j++) {
+
+    }
+} 
