48c47c380cdc333fa8b7488948d9a725ebce7ac5
Amer <amer2014np@gmail.com>
2023-02-07 21:12:08 +0100

    dajgi

---
+++ 44.cas/app.js
@@ -0,0 +1,18 @@
+//jos jedan nacin pravljenja objekta
+const objectFunctions = {
+    incrementScore: function(){
+        this.score+=1;
+    }
+}
+function createUser (firstName,lastName,score) {
+ const newUser=Object.create(objectFunctions)
+ newUser.firstName = firstName;
+ newUser.lastName = lastName;
+ newUser.score = score;
+ return newUser;
+}
+
+const tarik = createUser("Tarik","Ibrovic",16)
+console.log(tarik);
+tarik.incrementScore();
+console.log(tarik);
