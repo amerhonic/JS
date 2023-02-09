5c40173bd3e376417ef19c23b21fde411084d33a
Amer <amer2014np@gmail.com>
2023-02-09 20:59:23 +0100

    zaspat

--- 45.cas/2.js
+++ 45.cas/2.js
@@ -1,6 +1,10 @@
 arr1=[2,5,1,2,14];
 
-
-function wake(up,broj,pomeranje) {
-    
+const move=(arr,pos1,pos2) => {
+    const element = arr[pos1]
+    arr.splice(pos1,1);
+    arr.splice(pos2,0);
+    return arr;
 }
+
+console.log(move([10,20,30,40,50],0,2));
